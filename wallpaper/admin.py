d5c02540ad1283b5378332350c3b4f883fe235e0
mohammad <mohammadz270@yahoo.com>
2023-07-24 13:36:32 +0430

    contact model and wallpaper model added

--- wallpaper/admin.py
+++ wallpaper/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class WallpaperAdmin(admin.ModelAdmin):
+    list_display=['title','created_time','category']
+    date_hierarchy = 'created_time'
+    search_fields = ['title']
+    list_filter = ['category']
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display=['name','phone','created_time']
+    date_hierarchy = 'created_time'
+    search_fields = ['name','message']
+
+
+admin.site.register(Wallpaper,WallpaperAdmin)
+admin.site.register(Contact,ContactAdmin)
--- wallpaper/models.py
+++ wallpaper/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+class Wallpaper(models.Model):
+    title = models.CharField(max_length=225)
+    image = models.ImageField(upload_to='image/')
+    Wallpaper=models.ImageField(upload_to='walls/')
+    created_time = models.DateTimeField(auto_now=True)
+
+    
+    CATEGORY = [
+        ('GAMES' , 'Games'),
+        ('NATURE' , 'Nature'),
+        ('ABSTRACT' , 'Abstract'),
+        ('CARS' , 'Cars'),
+        ('CITY' , 'City') ,
+        ('MOVIES' , 'Movies'),
+        ('SPACE' , 'Space'), 
+        ('RANDOM' , 'Random'),
+    ]
+
+    category = models.CharField(max_length=10, choices=CATEGORY , default='Random')
+
+    def __str__(self) :
+        return self.title
+    
+
+
+class Contact(models.Model) :
+    name=models.CharField(max_length=100)
+    email=models.EmailField(max_length=254)
+    message=models.TextField()
+    phone=models.IntegerField()
+    created_time=models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+
+
