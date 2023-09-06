fc44691520bf781f9b000f7b43cf41a4ff536414
mohammad <mohammadz270@yahoo.com>
2023-09-06 12:48:36 +0430

    github

--- wallpaper/urls.py
+++ wallpaper/urls.py
@@ -6,14 +6,8 @@ urlpatterns = [
     path('',kome,name='home'),
     path('contact/',contact,name="contact"),
     path('collection/',collection,name='collection'),
-    path('games/',games,name='games'),
-    path('nature/',nature,name='nature'),
-    path('animals/',animals,name='animals'),
-    path('space/',space,name='space'),
-    path('abstract/',abstract,name='abstract'),
-    path('architecture/',architecture,name='architecture'),
-    path('cars/',cars,name='cars'),
     path('download/<int:file_id>/', download_file, name='download_file'),
     path('live_wallpapers/',live_wallpaper,name="live"),
-    path('download_livewallpapers/<int:num>/',d_live,name="d_live")
+    path('download_livewallpapers/<int:num>/',d_live,name="d_live"),
+    path('category/<str:name>/',category,name="category")
 ]
--- wallpaper/views.py
+++ wallpaper/views.py
@@ -14,8 +14,6 @@ def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            
-
             form.save()
             return render(request,'walls/success.html')  # Replace 'contact_success' with the URL name for the success page
     else:
@@ -30,110 +28,8 @@ def collection(request) :
 
 
 
-def nature(request,):
-    wall = Wallpaper.objects.filter(category="NATURE")
-    paginator = Paginator(wall, 3)  # Display 10 items per page
-    esm="NATURE"
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        "photolist" : wall,
-        "photocount" : wall.count(),
-        'page_obj': page_obj,
-        "esm": esm
-    }
-    return render(request ,'walls/category/caategory.html',context)
 
-def abstract(request,):
-    wall = Wallpaper.objects.filter(category="ABSTRACT")
-    paginator = Paginator(wall, 3)  
-    esm="ABSTRACT"
 
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        "photolist" : wall,
-        "photocount" : wall.count(),
-        'page_obj': page_obj,
-        "esm": esm
-    }
-    return render(request ,'walls/category/caategory.html',context)
-
-def cars(request,):
-    wall = Wallpaper.objects.filter(category="CARS")
-    paginator = Paginator(wall, 6)  # Display 10 items per page
-    esm="CARS"
-
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        "photolist" : wall,
-        "photocount" : wall.count(),
-        'page_obj': page_obj,
-        "esm": esm
-    }
-    return render(request ,'walls/category/caategory.html',context)
-
-def architecture(request,):
-    wall = Wallpaper.objects.filter(category="ARCHITECTURE")
-    paginator = Paginator(wall, 3)  # Display 10 items per page
-    esm="ARCHITECTURE"
-
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        "photolist" : wall,
-        "photocount" : wall.count(),
-        'page_obj': page_obj,
-        "esm": esm
-    }
-    return render(request ,'walls/category/caategory.html',context)
-
-def animals(request,):
-    wall = Wallpaper.objects.filter(category="ANIMALS")
-    paginator = Paginator(wall, 3)  # Display 10 items per page
-    esm="ANIMLAS"
-
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        "photolist" : wall,
-        "photocount" : wall.count(),
-        'page_obj': page_obj,
-        "esm": esm
-    }
-    return render(request ,'walls/category/caategory.html',context)
-
-def space(request,):
-    wall = Wallpaper.objects.filter(category="SPACE")
-    paginator = Paginator(wall, 3)  # Display 10 items per page
-    esm="SPACE"
-
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        "photolist" : wall,
-        "photocount" : wall.count(),
-        'page_obj': page_obj,
-        "esm": esm
-    }
-    return render(request ,'walls/category/caategory.html',context)
-
-def games(request,):
-    
-    wall = Wallpaper.objects.filter(category="GAMES")
-    paginator = Paginator(wall, 6)  # Display 10 items per page
-    esm="GAMES"
-
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    context = {
-        "photolist" : wall,
-        "photocount" : wall.count(),
-        'page_obj': page_obj,
-        "esm": esm
-    }
-    return render(request ,'walls/category/caategory.html',context)
 
 def download_file(request, file_id):
     my_file = get_object_or_404(Wallpaper, id=file_id)
@@ -142,6 +38,68 @@ def download_file(request, file_id):
     # Use Django's FileResponse to serve the file for download
     response = FileResponse(open(file_path, 'rb'))
     return response
+
+
+def category(request , name):
+    if name=="game":
+        wall = Wallpaper.objects.filter(category="GAMES")
+        paginator = Paginator(wall, 6)  # Display 10 items per page
+        esm="GAMES"
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+    elif name=="abstract" : 
+        wall = Wallpaper.objects.filter(category="ABSTRACT")
+        paginator = Paginator(wall, 6)  # Display 10 items per page
+        esm="ABSRTACT"
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+    
+    elif name=="architecture" : 
+        wall = Wallpaper.objects.filter(category="ARCHITECTURE")
+        paginator = Paginator(wall, 6)  # Display 10 items per page
+        esm="ARCHITECTURE"
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+    elif name=="nature" : 
+        wall = Wallpaper.objects.filter(category="NATURE")
+        paginator = Paginator(wall, 6)  # Display 10 items per page
+        esm="NATURE"
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+    elif name=="animals" : 
+        wall = Wallpaper.objects.filter(category="ANIMALS")
+        paginator = Paginator(wall, 6)  # Display 10 items per page
+        esm="ANIMALS"
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+    elif name=="cars" : 
+        wall = Wallpaper.objects.filter(category="CARS")
+        paginator = Paginator(wall, 6)  # Display 10 items per page
+        esm="CARS"
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+    elif name=="space" : 
+        wall = Wallpaper.objects.filter(category="SPACE")
+        paginator = Paginator(wall, 6)  # Display 10 items per page
+        esm="SPACE"
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+    
+    context = {
+        "photolist" : wall,
+        "photocount" : wall.count(),
+        'page_obj': page_obj,
+        "esm": esm
+    }
+
+    return render(request ,'walls/category/caategory.html',context)
+
+
 
 
 # def test(request,):
